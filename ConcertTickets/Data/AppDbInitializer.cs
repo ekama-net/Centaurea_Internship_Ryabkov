9c83252c5f4f60bee42f562b420ada2bc7a75562
ekama-net <maxim_rk1998@mail.ru>
2022-11-25 21:56:19 +0300

    Added navbar, search bar, dropdown menu for filtering by types. Made convenient display of the list

--- ConcertTickets/Data/AppDbInitializer.cs
+++ ConcertTickets/Data/AppDbInitializer.cs
@@ -13,6 +13,7 @@ namespace ConcertTickets
             {
                 var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
 
+                //context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
 
                 if (!context.PartyConcerts.Any())
@@ -21,25 +22,25 @@ namespace ConcertTickets
                     {
                         new PartyConcert()
                         {
-                            GroupOrArtistName = "QQQ",
+                            GroupOrArtistName = "Баста",
                             TicketsCount = 10,
-                            EventDate = new System.DateTime(2022,1,7),
-                            EventPlace = "Minsk",
+                            EventDate = new System.DateTime(2022,6,5),
+                            EventPlace = "Minsk Arena",
                             AgeLimit = 18,
-                            Price = 10,
+                            Price = 50,
                             Description = "1111111",
-                            ImageURL = "Later feature"
+                            ImageURL = "https://www.kvitki.by/imageGenerator/eventDetails/932bf3232a3cc5e3e1fdb23d78259579"
                         },
                         new PartyConcert()
                         {
-                            GroupOrArtistName = "WWW",
-                            TicketsCount = 10,
-                            EventDate = new System.DateTime(2022,1,8),
-                            EventPlace = "Minsk1",
-                            AgeLimit = 20,
-                            Price = 15,
+                            GroupOrArtistName = "Григорий Лепс",
+                            TicketsCount = 0,
+                            EventDate = new System.DateTime(2023,2,26),
+                            EventPlace = "Minsk Arena",
+                            AgeLimit = 18,
+                            Price = 50,
                             Description = "2222222",
-                            ImageURL = "Later feature"
+                            ImageURL = "https://www.kvitki.by/imageGenerator/eventDetails/4e2bd642b63bc9449a0c5c6e4857b252"
                         }
                     });
                     context.SaveChanges();
@@ -50,16 +51,36 @@ namespace ConcertTickets
                     {
                         new ClassicalConcert()
                         {
-                            GroupOrArtistName = "ClassicQ",
-                            TicketsCount = 10,
-                            EventDate = new System.DateTime(2022,1,10),
-                            EventPlace = "MinskClassicQ",
-                            VoiceType = VoiceType.Бас,
-                            ConcertName = "ClassicQConcert",
-                            СomposerName = "Vasya",
-                            Price = 5,
-                            Description = "1111111",
-                            ImageURL = "Later feature"
+                            GroupOrArtistName = "Classic",
+                            TicketsCount = 5,
+                            EventDate = new System.DateTime(2023,1,10),
+                            EventPlace = "Minsk",
+                            VoiceType = VoiceType.Сопрано,
+                            ConcertName = "Classic",
+                            СomposerName = "Vasiliy",
+                            Price = 30,
+                            Description = "3333333",
+                            ImageURL = "https://www.kvitki.by/imageGenerator/eventDetails/b3b9c7b672c006d88ed815c3ba5771da"
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.OpenAirConcerts.Any())
+                {
+                    context.OpenAirConcerts.AddRange(new List<OpenAirConcert>()
+                    {
+                        new OpenAirConcert()
+                        {
+                            GroupOrArtistName = "Minsk Sea beach 2",
+                            TicketsCount = 11,
+                            EventDate = new System.DateTime(2023,5,22),
+                            EventPlace = "Minsk",
+                            HeadLiner = "DJ Egor",
+                            HowToGetTo = "village Braslau -> southern side of beach",
+                            Price = 20,
+                            Description = "44444444",
+                            ImageURL = "http://sun9-85.userapi.com/s/v1/if2/h4GchSmMxoPkH-8ORiUEF1ColcedCEfDQPrzUWNeLR56LiAZYI4XifzIXhEh6rTYiPM8dpA47uqLwP7by2pXfsJC.jpg?size=200x294&quality=96&crop=0,0,696,1024&ava=1"
                         }
                     });
                     context.SaveChanges();
--- ConcertTickets/Models/Concert.cs
+++ ConcertTickets/Models/Concert.cs
@@ -7,16 +7,12 @@ namespace ConcertTickets
     {
         [Key]
         public int ConcertId { get; set; }
-        [Display (Name ="Name")]
         public string GroupOrArtistName { get; set; }
         public int TicketsCount { get; set; }
-        [Display(Name = "When")]
         public DateTime EventDate { get; set; }
-        [Display(Name = "Where")]
         public string EventPlace { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
-        [Display(Name = "Banner")]
         public string ImageURL { get; set; }
 
 
