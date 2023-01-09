0a98ba1ddba9ec01f2e52843d00fc370060f420b
ekama-net <maxim_rk1998@mail.ru>
2023-01-09 23:22:51 +0300

    minor changes

--- ConcertTickets/Startup.cs
+++ ConcertTickets/Startup.cs
@@ -49,7 +49,7 @@ namespace ConcertTickets
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Concert}/{action=Index}/{id?}");
             });
 
             AppDbInitializer.Seed(app);
